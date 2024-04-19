16d92501dea70ba281b7d879a8df93245221e0e2
DESKTOP-F4BC13L\hi <xodud6809@naver.com>
2024-04-19 18:07:29 +0900

    cccc

--- src/JDBC/RentList_Select.java
+++ src/JDBC/RentList_Select.java
@@ -5,7 +5,6 @@ import java.util.ArrayList;
 
 public class RentList_Select {
 	public static void main(String[] args) {
-		
 		RentDao rdao = RentDao.getInstance();
 		ArrayList<RentDto> list = rdao.selectRent();
 		
